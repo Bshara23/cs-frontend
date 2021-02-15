9e66c9862881e90adff9c0302393048062398368
Bshara Zahran <bshara23@gmail.com>
2021-02-15 14:54:11 +0200

    added 404 page

--- src/pages/E404Page.jsx
+++ src/pages/E404Page.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import {Player, Controls} from '@lottiefiles/react-lottie-player';
 
-export default function E404Page() {
-    return (
-        <div>
-            <h1>E404Page</h1>
-        </div>
-    )
+export default function E404Page () {
+  return (
+    <div>
+      <Player
+        src="https://assets7.lottiefiles.com/temp/lf20_USCruP.json"
+        background="transparent"
+        speed="1"
+        loop
+        autoplay
+        style={{height: '500px', width: '500px'}}
+      />
+
+    </div>
+  );
 }
